16063822cf3a1779901225a4cda2fc4998dd8a8b
Zemaaan <zemaaan@live.com>
2019-04-18 22:40:40 +0200

    Dodane opcije za korisnika, gasenje/ paljenje nastavak ne radi.

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ namespace Preglednik
             return nastavak_datoteke + " datoteka";
         }
 
-        static public void ispisi_velicinu(float? kolicina_u_bytovima, string ime_datoteke)
+        static public void ispisi_velicinu(float? kolicina_u_bytovima, string ime_datoteke, bool nastavci)
         {
 
             const int gigabyte = 1000000000; // velicina u bitovima
@@ -75,8 +75,17 @@ namespace Preglednik
             // {
             //     Console.WriteLine("| {0,-60} {1,10} {2,11} {3,14} {4,-190}", ime_datoteke,"≈ 0", String.Format("{0:0.00}", kolicina_u_bytovima / (1024 * 1024)), String.Format("{0:0.00}", kolicina_u_bytovima / 1024), "    " + ispisi_tip_datoteke(ime_datoteke));
             // }
-            Console.WriteLine("| {0,-60} {1,10} {2,11} {3,14} {4,-60}", ime_datoteke, String.Format("{0:0.0000}", kolicina_u_bytovima / (1024 * 1024 * 1024)), String.Format("{0:0.00}", kolicina_u_bytovima / (1024 * 1024)), String.Format("{0:0.00}", kolicina_u_bytovima / 1024 ), "   " + ispisi_tip_datoteke(ime_datoteke));
-            
+
+            if (nastavci)
+            {
+                Console.WriteLine("| {0,-60} {1,10} {2,11} {3,14} {4,-60}", ime_datoteke, String.Format("{0:0.0000}", kolicina_u_bytovima / (1024 * 1024 * 1024)), String.Format("{0:0.00}", kolicina_u_bytovima / (1024 * 1024)), String.Format("{0:0.00}", kolicina_u_bytovima / 1024), "   " + ispisi_tip_datoteke(ime_datoteke));
+            }
+
+            else
+            {
+                Console.WriteLine("| {0,-60} {1,10} {2,11} {3,14} {4,-60}", Path.GetFileNameWithoutExtension(ime_datoteke), String.Format("{0:0.0000}", kolicina_u_bytovima / (1024 * 1024 * 1024)), String.Format("{0:0.00}", kolicina_u_bytovima / (1024 * 1024)), String.Format("{0:0.00}", kolicina_u_bytovima / 1024), "   " + ispisi_tip_datoteke(ime_datoteke));
+            }
+
 
             // else if (kolicina_u_bytovima < gigabyte && kolicina_u_bytovima > megabyte)
             // {
@@ -100,6 +109,8 @@ namespace Preglednik
             static void Main(string[] args)
         {
             string direktorij = null; // opcenito podrucje za postavljanja varijabli zbog try catch blokova
+            string naredba = null;
+
             FileInfo[] datoteke = null;
             float velicina = 0;
 
@@ -107,52 +118,109 @@ namespace Preglednik
             direktorij = popis_diskova[0].ToString(); // odaberemo prvi disk kao default putanju, u slucaju da korisnik pokusa upisati direktorij koji ne postoji
                                                       // obicno C:/ disk, ali ne mora nuzno biti
 
+            bool ispisi_nastavke = false;
+            bool list_dir_nacin_rada = false;
 
             Console.SetWindowSize(1, 1);
             Console.SetBufferSize(300, 100);
             Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
 
-            Console.WriteLine("Upisite direktorij:");
-            direktorij = Console.ReadLine();
+            Console.WriteLine("Upisite naredbu:");
+            naredba = Console.ReadLine();
 
-            DirectoryInfo podaci_o_direktoriju = new DirectoryInfo(direktorij);
-
-            try
+            if(naredba == "/?")
             {
-                datoteke = podaci_o_direktoriju.GetFiles();
+                Console.WriteLine("Popis mogucih naredbi.");
+                Console.WriteLine("(/?) -- Pomoć ");
+                Console.WriteLine("list_dir -- Nacin sa rad sa direktorijima, nakon čega se upisuje željeni direktorij.");
+                Console.WriteLine("exit -- naredba za izlazak iz određenog načina rada.");
+                Console.WriteLine("set_extensions(false) - aktiviranje ispisa datoteka bez ekstenzija.");
+                Console.WriteLine("set_extensions(true) - aktiviranje ispisa datoteka sa ekstenzijama.");
             }
-            catch (System.IO.DirectoryNotFoundException)
+            else if (naredba == "set_extensions(false)")
             {
+                if (!ispisi_nastavke)
+                {
+                    Console.WriteLine("Produzenja datoteka vec su ugasene.");
+                    
+                }
 
-                Console.WriteLine("Taj direktorij ne postoji."); Main(null);
-            } // vrtimo se tako dugo, dok korisnik ne upise ispravan direktorij
-
-            if (datoteke.Length == 0)
-            {
-                Console.WriteLine("U direktoriju ne postoje datoteke.");
-                Main(null);
+                else
+                {
+                    Console.WriteLine("Nadalje se neće ispisivati produzenja datoteka.");
+                    ispisi_nastavke = false;
+                }
             }
 
-            Console.WriteLine("+------------------------------------------------------------------------+-----------+--------------+------------+-----------------------------+");
-            Console.WriteLine("| Naziv datoteke:                                       |             GB |        MB |           KB |   Vrsta datoteke:                        |");
-            Console.WriteLine("+------------------------------------------------------------------------+-----------+--------------+------------+-----------------------------+");
-
-            foreach (FileInfo trenutna_datoteka in datoteke)
+            else if (naredba == "set_extensions(true)")
             {
-                velicina += trenutna_datoteka.Length;
-                ispisi_velicinu(trenutna_datoteka.Length, trenutna_datoteka.FullName);
-                ispisi_tip_datoteke(trenutna_datoteka.FullName);
-            }
-            Console.WriteLine("+--------------------+-------------+---------+-------------------------------------------------+");
-            Console.WriteLine("|{0, 15} B | {1, 13} KB | {2, 4} MB |                                          |",
-                velicina,
-                velicina / 1024,
-                velicina / (1024 * 1024));
-            Console.WriteLine("+------------------+-------------+---------+---------------------------------------------------+");
+                if (ispisi_nastavke)
+                {
+                    Console.WriteLine("Produzenja datoteka su vec upaljene.");
+                }
 
+                else
+                {
+                    Console.WriteLine("Program ce nadalje ispisivati imena datoteka sa nastavcima.");
+                    ispisi_nastavke = true;
+                }
+            }
+
+            else if(naredba == "list_dir")
+            {
+                list_dir_nacin_rada = true;
+                while (list_dir_nacin_rada)
+                {
+                    
+                    Console.WriteLine("[list_dir] Upisite direktorij:");
+                    direktorij = Console.ReadLine();
+
+                    if (direktorij == "exit")
+                    {
+                        list_dir_nacin_rada = false;
+                        break;
+                    }
+
+                    DirectoryInfo podaci_o_direktoriju = new DirectoryInfo(direktorij);
+
+                    try
+                    {
+                        datoteke = podaci_o_direktoriju.GetFiles();
+                    }
+                    catch (System.IO.DirectoryNotFoundException)
+                    {
+
+                        Console.WriteLine("Taj direktorij ne postoji."); Main(null);
+                    } // vrtimo se tako dugo, dok korisnik ne upise ispravan direktorij
+
+                    if (datoteke.Length == 0)
+                    {
+                        Console.WriteLine("U direktoriju ne postoje datoteke.");
+                        Main(null);
+                    }
+
+                    Console.WriteLine("+------------------------------------------------------------------------+-----------+--------------+------------+-----------------------------+");
+                    Console.WriteLine("| Naziv datoteke:                                       |             GB |        MB |           KB |   Vrsta datoteke:                        |");
+                    Console.WriteLine("+------------------------------------------------------------------------+-----------+--------------+------------+-----------------------------+");
+
+                    foreach (FileInfo trenutna_datoteka in datoteke)
+                    {
+                        velicina += trenutna_datoteka.Length;
+                        ispisi_velicinu(trenutna_datoteka.Length, trenutna_datoteka.FullName, ispisi_nastavke);
+                        ispisi_tip_datoteke(trenutna_datoteka.FullName);
+                    }
+                    Console.WriteLine("+--------------------+-------------+---------+-------------------------------------------------+");
+                    Console.WriteLine("|{0, 15} B | {1, 13} KB | {2, 4} MB |                                          |",
+                        velicina,
+                        velicina / 1024,
+                        velicina / (1024 * 1024));
+                    Console.WriteLine("+------------------+-------------+---------+---------------------------------------------------+");
+
+                }
+            }
             // Console.SetCursorPosition(1, 3);
             // Console.Write(">");
-            int brojRedova = datoteke.Length + 6;
+            // int brojRedova = datoteke.Length + 6;
 
             Main(null); // ovo je bilo za ocekivati..
 
